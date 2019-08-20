43e84a02759f47b69ec6b276aba87ca5a321b609
Ilham <48025760+ipirm@users.noreply.github.com>
2019-08-20 04:34:18 +0400

    message

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+
+document.querySelector('.burger-menu').addEventListener("click", function(){
+    document.querySelector('.header-nav-mobile').classList.toggle('active-menu');
+});
