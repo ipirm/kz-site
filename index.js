2714848b3ab3799404c5d043d87bcac64bf91450
User <ilham.pirm@gmail.com>
2019-10-03 02:12:18 +0400

    first commit!

---
+++ index.js
@@ -0,0 +1,5 @@
+require('simple-git')()
+    .add('./*')
+    .commit("first commit!")
+    .addRemote('origin', 'https://github.com/ipirm/kz-site.git')
+    .push(['-u', 'origin', 'master'], () => console.log('done'));
